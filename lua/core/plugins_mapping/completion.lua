cb3e448536fb16a31a04d2edbd202a6c77517ed5
X-Yunocchi-X <syf2003_bool@163.com>
2023-12-19 21:25:42 +0800

    update keymap(toggleterm)

--- lua/core/plugins_mapping/completion.lua
+++ lua/core/plugins_mapping/completion.lua
@@ -3,4 +3,4 @@ local keymap = vim.keymap
 keymap.set("n", "g[", ":Lspsaga  diagnostic_jump_prev<cr>")
 keymap.set("n", "g]", ":Lspsaga  diagnostic_jump_next<cr>")
 keymap.set("n", "K", ":Lspsaga  hover_doc<cr>")
-keymap.set("n", "gD", ":Lspsaga  goto_definition<cr>")
+keymap.set("n", "gd", ":Lspsaga  goto_definition<cr>")
--- lua/core/plugins_mapping/tool.lua
+++ lua/core/plugins_mapping/tool.lua
@@ -3,8 +3,8 @@ local keymap = vim.keymap
 keymap.set("n", "<c-e>", "<cmd>NvimTreeToggle<cr>")
 
 -- ToggleTerm
-keymap.set("n", "<c-\\>", "<cmd>ToggleTerm<cr>")
-keymap.set("t", "<c-\\>", "<cmd>ToggleTerm<cr>")
+keymap.set("n", "<c-\\>", "<cmd>ToggleTerm	direction=horizontal<cr>")
+keymap.set("t", "<c-\\>", "<cmd>ToggleTerm	direction=horizontal<cr>")
 keymap.set("n", "<a-\\>", "<cmd>ToggleTerm  direction=float<cr>")
 keymap.set("t", "<a-\\>", "<cmd>ToggleTerm  direction=float<cr>")
 keymap.set("t", "<Esc>", "<c-\\><c-n>")
