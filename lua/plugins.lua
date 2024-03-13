087ffa902674d1573d3e51cf392614a8d2728c1d
X-Yunocchi-X <syf2003_bool@163.com>
2024-03-13 14:03:43 +0800

    add which-key.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -57,6 +57,16 @@ require("lazy").setup({
 		},
 	},
 
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {}
+	},
+
 	--------------------- debug ----------------------
 	{
 		"rcarriga/nvim-dap-ui",
